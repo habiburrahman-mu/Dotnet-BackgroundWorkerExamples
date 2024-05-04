c59cd6af4d1c0ded9affc6804a27b63d55a21cdd
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2024-05-05 00:09:43 +0600

    Configured the hangfire service and hangfire database

--- HangfireExample/Program.cs
+++ HangfireExample/Program.cs
@@ -1,7 +1,28 @@
+using Hangfire;
+using Hangfire.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var hangfireConnectionString = builder.Configuration.GetConnectionString("HangfireConnection");
+
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(
+        hangfireConnectionString,
+        new SqlServerStorageOptions
+        {
+            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+            QueuePollInterval = TimeSpan.Zero,
+            UseRecommendedIsolationLevel = true,
+            DisableGlobalLocks = true,
+        }));
+
+builder.Services.AddHangfireServer();
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,5 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseHangfireDashboard("/dashboard");
+app.MapControllers();
 
 app.Run();
